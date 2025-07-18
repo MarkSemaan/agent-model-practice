1ade90474ce1b0606206890d440aa89ee277bb32
MarkSemaan <mark.h.semaan@gmail.com>
2025-07-18 21:36:11 +0300

    refactored readme and updated agent methods.

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -3,17 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Agent extends Model
 {
-    protected $fillable = [
+    use SoftDeletes;
+
+    protected $fillable = [ //Excludes important fields from mass assignment, like id
         'name',
         'description',
         'type',
         'is_active',
     ];
 
-    protected $casts = [
+    protected $casts = [ // Get the attributes that should be cast
         'is_active' => 'boolean',
     ];
 }
